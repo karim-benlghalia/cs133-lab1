639a87c9aaa41de9a734d3c2d7660cb4c7170481
ben-karim2014 <k.benlghalia@gmail.com>
2021-01-19 13:17:06 -0800

    ....

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -1,13 +1,16 @@
 // Header inclusions, if any...
-
+#include <omp.h>
 #include "lib/gemm.h"
-
+#include <cstring>
 // Using declarations, if any...
-
+ 
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ]) {
+// float temp;
+// int i, j, k;
+// int nCores=omp_get_max_threads();
+// omp_set_num_threads(nCores);
 
- 
-  // Your code goes here...
+
 
 }
