707edd88e6a033341295ef9e23adaf1dbae96e12
ben-karim2014 <k.benlghalia@gmail.com>
2021-01-18 22:06:46 -0800

    lab1: first version

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -6,5 +6,8 @@
 
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ]) {
+
+ 
   // Your code goes here...
+
 }
--- lab1/omp.cpp
+++ lab1/omp.cpp
@@ -1,10 +1,28 @@
 // Header inclusions, if any...
-
+#include <cstring>
 #include "lib/gemm.h"
+#include <omp.h>
 
 // Using declarations, if any...
 
 void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
                   float c[kI][kJ]) {
-  // Your code goes here...
+  float temp;
+  int i, j, k;
+ //int nCores=omp_get_max_threads();
+  omp_set_num_threads(64);
+   for (int i = 0; i < kI; ++i) {
+    std::memset(c[i], 0, sizeof(float) * kJ);
+  }
+
+  #pragma omp parallel for private(k,j,temp) 
+  for (int i = 0; i < kI; ++i) {
+    for (int k = 0; k < kK; ++k) {
+      temp=a[i][k];
+      for (int j = 0; j < kJ; ++j) {
+        c[i][j] += temp*b[k][j];
+      }
+    }
+  }
+  
 }
