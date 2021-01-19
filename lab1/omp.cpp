4421162923d321ddd9eb4e769259208c398a898e
ben-karim2014 <k.benlghalia@gmail.com>
2021-01-19 14:44:38 -0800

    ....

--- lab1/omp.cpp
+++ lab1/omp.cpp
@@ -13,12 +13,12 @@ void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
  int nCores=omp_get_max_threads();
  omp_set_num_threads(nCores);
 
- #pragma omp parallel for
+ #pragma omp parallel for private(i) schedule(static)
    for (int i = 0; i < kI; ++i) {
     std::memset(c[i], 0, sizeof(float) * kJ);
   }
 
-  #pragma omp parallel for private(i,k,j,temp) schedule(static)
+  #pragma omp parallel for private(i,k,j,temp) schedule(static, 16)
   for (int i = 0; i < kI; ++i) {
     for (int k = 0; k < kK; ++k) {
       temp=a[i][k];
