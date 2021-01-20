492a37eaa860b9025a316b07966fc41dfc84efc2
ben-karim2014 <k.benlghalia@gmail.com>
2021-01-19 22:36:41 -0800

    ....

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -3,50 +3,47 @@
 #include "lib/gemm.h"
 #include <cstring>
 // Using declarations, if any...
-#define blocking_size 64
+#define blocking_size 32
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ])
 {
-  int nCores=omp_get_max_threads();
- omp_set_num_threads(nCores);
+  
+  int nCores = omp_get_max_threads();
+  omp_set_num_threads(nCores);
+  int i, j, k;
+  int bi, bj, bk;
+  int i_blocks = kI / blocking_size;
+  int j_blocks = kJ / blocking_size;
 
-  int i, j, k, i_block, j_block, k_block;
-  float temp;
-  float temp_g[blocking_size][blocking_size] = {0};
-  //int num;
-  //int id;
+#pragma omp parallel for private(i, j, k, bi, bj) schedule(static, 16)
 
-#pragma omp  parallel  for private(k_block, j_block, i, k, j, temp_g) schedule(static)
-  for (i_block = 0; i_block < kI; i_block += blocking_size)
+  for (i = 0; i < i_blocks; i++)
   {
-    
-    for (j_block = 0; j_block < kJ; j_block += blocking_size)
-    {
-        float temp_buff[blocking_size][blocking_size] = {0};
-      for (k_block = 0; k_block < kK; k_block += blocking_size)
-      {
-        for (i = i_block; i < i_block + blocking_size; ++i)
-        {
-          for (k = k_block; k < k_block + blocking_size; ++k)
-          {
 
-            temp = a[i][k];
-            for (j = j_block; j < j_block + blocking_size; ++j)
-            {
-              
-              int indexI = i - i_block;
-              int indexJ = j - j_block;
-              temp_buff[indexI][indexJ] = temp_buff[indexI][indexJ] + temp * b[k][j];
-              temp_g[indexI][indexJ] =temp_buff[indexI][indexJ];
-            }
+    for (j = 0; j < j_blocks; j++)
+    {
+
+      float temp[blocking_size][blocking_size];
+      memset(temp, 0, sizeof(float) * blocking_size * blocking_size);
+
+      for (k = 0; k < kK; k++)
+      {
+        for (bi = 0; bi < blocking_size; bi++)
+        {
+          for (bj = 0; bj < blocking_size; bj++)
+          {
+            temp[bi][bj] += a[i * blocking_size + bi][k] * b[k][j * blocking_size + bj];
           }
         }
       }
-      for (i = 0; i < blocking_size; i++)
-       { int index = i_block + i;
-         c[index][j_block] = temp_g[i][0];
-         }
+
+      for (bi = 0; bi < blocking_size; bi++)
+      {
+        for (bj = 0; bj < blocking_size; bj++)
+        {
+          c[i * blocking_size + bi][j * blocking_size + bj] = temp[bi][bj];
+        }
+      }
     }
   }
-
 }
