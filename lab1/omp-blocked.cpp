dcdb074d5b4cab805b4e338422169b74dfb49c78
ben-karim2014 <k.benlghalia@gmail.com>
2021-01-19 20:13:15 -0800

    ....

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -12,38 +12,44 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
 
   int i, j, k, i_block, j_block, k_block;
   float temp;
+  float temp_g[blocking_size][blocking_size] = {0};
   //int num;
   //int id;
 
-#pragma omp parallel private(k_block, j_block, i, k, j)
+#pragma omp parallel private(k_block, j_block, i, k, j, temp_g)
 
 {
  
  #pragma omp for schedule(static)
   for (i_block = 0; i_block < kI; i_block += blocking_size)
   {
+    
     for (j_block = 0; j_block < kJ; j_block += blocking_size)
     {
-      float temp_buff[blocking_size][blocking_size] = {0};
-
+        float temp_buff[blocking_size][blocking_size] = {0};
       for (k_block = 0; k_block < kK; k_block += blocking_size)
       {
-        for (i = i_block; i < i_block + blocking_size; i++)
+        for (i = i_block; i < i_block + blocking_size; ++i)
         {
-          for (k = k_block; k < k_block + blocking_size; k++)
+          for (k = k_block; k < k_block + blocking_size; ++k)
           {
+
             temp = a[i][k];
-            for (j = j_block; j < j_block + blocking_size; j++)
+            for (j = j_block; j < j_block + blocking_size; ++j)
             {
+              
               int indexI = i - i_block;
               int indexJ = j - j_block;
-              temp_buff[indexI][indexJ] += temp * b[k][j];
+              temp_buff[indexI][indexJ] = temp_buff[indexI][indexJ] + temp * b[k][j];
+              
             }
           }
         }
       }
       for (i = 0; i < blocking_size; i++)
-        c[i_block + i][j_block] = temp_buff[i][0];
+       { int index = i_block + i;
+         c[index][j_block] = temp_buff[i][0];
+         }
     }
   }
 }
