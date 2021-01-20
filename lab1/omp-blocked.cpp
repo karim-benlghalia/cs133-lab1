512cfa6871f8fa568d4256c272552bf2fdd1d8ca
ben-karim2014 <k.benlghalia@gmail.com>
2021-01-19 17:53:42 -0800

    ....

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -3,14 +3,50 @@
 #include "lib/gemm.h"
 #include <cstring>
 // Using declarations, if any...
- 
+ #define blocking_size 32
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ]) {
-// float temp;
-// int i, j, k;
-// int nCores=omp_get_max_threads();
-// omp_set_num_threads(nCores);
+omp_set_num_threads(32);
+	int i, j, k;
+    int bi, bj, bk;
+    int i_blocks = kI/blocking_size;
+    int j_blocks = kJ/blocking_size;
+     int k_blocks = kK/blocking_size;
+
+    #pragma omp parallel  private(i, j, k, bi, bj) 
+    {
+
+    #pragma omp for schedule(static)
+    for(i = 0; i < i_blocks; i++) {
 
 
+        for(k = 0; k < j_blocks; k++) {
+
+           float temp[blocking_size][blocking_size];
+           memset(temp,0,sizeof(float)*blocking_size*blocking_size);
+
+            for(j = 0; j < kJ; j++) {
+                for(bi = 0; bi < blocking_size; bi++) {
+                  int calc_bloc = i*blocking_size + bi;
+                    for(bk = 0; bk < blocking_size; bk++) {
+                        temp[bi][bk] = temp[bi][bk] + a[ calc_bloc ][j]*b[j][k*blocking_size + bk];
+                         c[calc_bloc][k*blocking_size + bk] = temp[bi][bk];
+                    }
+                }
+            }
+
+            // for(bi = 0; bi < blocking_size; bi++) {
+            //    int calc_bloc = i*blocking_size + bi;
+            //     for(bk = 0; bk < blocking_size; bk++) {
+            //         c[calc_bloc][k*blocking_size + bk] = temp[bi][bk];
+            //     }
+            // }
+        }
+
+
+
+
+    }
+    }
 
 }
--- lab1/omp.cpp
+++ lab1/omp.cpp
@@ -13,7 +13,7 @@ void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
  int nCores=omp_get_max_threads();
  omp_set_num_threads(nCores);
 
- #pragma omp parallel for private(i) schedule(static)
+ #pragma omp parallel for private(i)
    for (int i = 0; i < kI; ++i) {
     std::memset(c[i], 0, sizeof(float) * kJ);
   }
@@ -23,7 +23,7 @@ void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
     for (int k = 0; k < kK; ++k) {
       temp=a[i][k];
       for (int j = 0; j < kJ; ++j) {
-        c[i][j] += a[i][k]*b[k][j];
+        c[i][j] += temp*b[k][j];
       }
     }
   }
